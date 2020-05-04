02ecea62212539914d00b8a9c1ae307572501931
Ayush Gemini <ayushgemini13@gmail.com>
2020-05-04 15:33:34 +0530

    Added -- urls.py

--- blogapi/blog_project/posts/views.py
+++ blogapi/blog_project/posts/views.py
@@ -10,6 +10,6 @@ class PostList(generics.ListAPIView):
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
